4daa861b8462c6eaaa17ab0a4f194c152fa35913
Frankma17 <53158365+Frankma17@users.noreply.github.com>
2022-03-24 08:15:03 -0500

    Prueba 1

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
 // 1) Conexion
-$db_server = "192.168.67.2:30714";
-$db_user = "root";
-$db_pass = "root";
-$db_name = "nodos";
-$mysqli = new mysqli($db_server, $db_user, $db_pass, $db_name);
-if ($conexion = $mysqli){
-
-
+if ($conexion = new mysqli("192.168.67.2:30714", "root", "root",'nodos')){
 echo "<p>MySQL le ha dado permiso a PHP para ejecutar consultas con ese usuario</p>";
 
 // 2) Preparar la orden SQL
-$consulta= "SELECT*FROM nodo WHERE id=1";
+$consulta= "SELECT*FROM nodo WHERE id = 1";
 
 // 3) Ejecutar la orden y obtener datos
 mysqli_select_db($conexion,"nodos");
@@ -25,8 +18,3 @@ echo "-"; // un separador
 echo $fila["nombre"];
 echo "</p>";
 }
-
-}else{
-echo "<p> MySQL no conoce ese usuario y password</p>";
-}
-?>
