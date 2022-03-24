f717fe9a5b1a9bb5b9f4c574f23742ac51c716c4
Frankma17 <53158365+Frankma17@users.noreply.github.com>
2022-03-24 06:52:56 -0500

    Update PHP Mysql

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <?php
-// 1) Conexión
-if ($conexión = new mysqli("192.168.67.2", "root", "root",'nodos')){
+// 1) Conexion
+if ($conexion = new mysqli("192.168.67.2:30714", "root", "root",'nodos')){
 echo "<p>MySQL le ha dado permiso a PHP para ejecutar consultas con ese usuario</p>";
 
 // 2) Preparar la orden SQL
-$consulta= "SELECT*FROM nodo";
+$consulta= "SELECT*FROM nodo WHERE id = 1";
 
 // 3) Ejecutar la orden y obtener datos
-mysql_select_db("nodos");
-$datos= mysql_query ($consulta);
+mysqli_select_db($conexion,"nodos");
+$datos= mysqli_query ($conexion,$consulta);
 
 // 4) Ir Imprimiendo las filas resultantes
-while ($fila =mysql_fetch_array($datos)){
+while ($fila =mysqli_fetch_array($datos)){
 echo "<p>";
 echo $fila ["id"];
 echo "-"; // un separador
 echo $fila["nombre"];
-echo "-"; // un separador
 echo "</p>";
 }
 
