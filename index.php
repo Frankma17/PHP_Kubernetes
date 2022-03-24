4fb6bccf253243960bae35b2fe4eb7ee1cc9534f
Frankma17 <53158365+Frankma17@users.noreply.github.com>
2022-03-24 08:30:50 -0500

    asd

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 // 1) Conexión
-if ($conexión = mysql_connect("192.168.67.2", "root", "root")){
+if ($conexión = mysqli_connect("192.168.67.2", "root", "root")){
 echo "<p>MySQL le ha dado permiso a PHP para ejecutar consultas con ese usuario</p>";
 
 // 2) Preparar la orden SQL
 $consulta= "SELECT*FROM nodo";
 
 // 3) Ejecutar la orden y obtener datos
-mysql_select_db("nodos");
-$datos= mysql_query ($conexion,$consulta);
+mysqli_select_db("nodos");
+$datos= mysqli_query ($conexion,$consulta);
 
 // 4) Ir Imprimiendo las filas resultantes
-while ($fila =mysql_fetch_array($datos)){
+while ($fila =mysqli_fetch_array($datos)){
 echo "<p>";
 echo $fila ["id"];
 echo "-"; // un separador
