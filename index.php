40fdcedb7f69564facee935ec85e4a7ff5a5173b
Frankma17 <53158365+Frankma17@users.noreply.github.com>
2022-03-24 04:21:34 -0500

    Add index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// 1) Conexión
+if ($conexión = mysql_connect("192.168.67.2", "root", "root")){
+echo "<p>MySQL le ha dado permiso a PHP para ejecutar consultas con ese usuario</p>";
+
+// 2) Preparar la orden SQL
+$consulta= "SELECT*FROM nodo";
+
+// 3) Ejecutar la orden y obtener datos
+mysql_select_db("nodos");
+$datos= mysql_query ($consulta);
+
+// 4) Ir Imprimiendo las filas resultantes
+while ($fila =mysql_fetch_array($datos)){
+echo "<p">;
+echo $fila ["id"];
+echo "-"; // un separador
+echo $fila["nombre"];
+echo "-"; // un separador
+echo "</p>";
+}
+
+}else{
+echo "<p> MySQL no conoce ese usuario y password</p>";
+}
+?>
