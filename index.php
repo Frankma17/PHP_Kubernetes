e74399762990d5b0fcb9bd6300428f31393ada2d
Frankma17 <53158365+Frankma17@users.noreply.github.com>
2022-03-24 08:22:13 -0500

    Corregir muchas veces

--- index.php
+++ index.php
@@ -1,21 +1,26 @@
 <?php
-// 1) Conexion
-if ($conexion = new mysqli("192.168.67.2:30714", "root", "root",'nodos')){
+// 1) Conexión
+if ($conexión = mysql_connect("192.168.67.2", "root", "root")){
 echo "<p>MySQL le ha dado permiso a PHP para ejecutar consultas con ese usuario</p>";
 
 // 2) Preparar la orden SQL
-$consulta= "SELECT*FROM nodo WHERE id = 1";
+$consulta= "SELECT*FROM nodo";
 
 // 3) Ejecutar la orden y obtener datos
-mysqli_select_db($conexion,"nodos");
-$datos= mysqli_query ($conexion,$consulta);
+mysql_select_db("nodos");
+$datos= mysql_query ($conexion,$consulta);
 
 // 4) Ir Imprimiendo las filas resultantes
-while ($fila =mysqli_fetch_array($datos)){
-echo "<p>";
+while ($fila =mysql_fetch_array($datos)){
+echo "<p">;
 echo $fila ["id"];
 echo "-"; // un separador
 echo $fila["nombre"];
+echo "-"; // un separador
 echo "</p>";
+}
+
+}else{
+echo "<p> MySQL no conoce ese usuario y password</p>";
 }
 ?>
