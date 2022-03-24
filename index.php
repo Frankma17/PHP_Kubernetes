0fafa05ab008660e88f5ace7fef6f273a02a8082
Frankma17 <53158365+Frankma17@users.noreply.github.com>
2022-03-24 08:45:07 -0500

    Cambio-Comprobado

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 // 1) Conexión
-if ($conexión = mysqli_connect("192.168.67.2:30714", "root", "root")){
+if ($conexion = new mysqli("192.168.67.2:30714", "root", "root","nodos")){
 echo "<p>MySQL le ha dado permiso a PHP para ejecutar consultas con ese usuario</p>";
 
 // 2) Preparar la orden SQL
 $consulta= "SELECT*FROM nodo";
 
 // 3) Ejecutar la orden y obtener datos
-mysqli_select_db("nodos");
+mysqli_select_db($conexion,"nodos");
 $datos= mysqli_query ($conexion,$consulta);
 
 // 4) Ir Imprimiendo las filas resultantes
